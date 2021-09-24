22c801a9154203e21c6d5f95d79ba158423dc9a2
Smiling Dog <a.kudr74@gmail.com>
2021-09-24 19:14:56 +0300

    changed selectors

--- js.js
+++ js.js
@@ -1,16 +1,16 @@
-var image1 = document.getElementsByClassName('parallaxImg1');
+var image1 = document.querySelector('.parallaxImg1');
 new simpleParallax(image1, {
     orientation: "left"
 });
-var image2 = document.getElementsByClassName('parallaxImg2');
+var image2 = document.querySelector('.parallaxImg2');
 new simpleParallax(image2, {
     orientation: "right"
 });
-var image3 = document.getElementsByClassName('parallaxImg3');
+var image3 = document.querySelector('.parallaxImg3');
 new simpleParallax(image3, {
     orientation: "up"
 });
-var image4 = document.getElementsByClassName('parallaxImg4');
+var image4 = document.querySelector('.parallaxImg4');
 new simpleParallax(image4, {
     orientation: "down"
 });
